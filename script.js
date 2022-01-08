8fd1c193ccb51a214ed04b2cb2e10af55954514c
Evan Chernicky <echernicky@gmail.com>
2022-01-08 12:50:38 -0500

    Added typing effect, slide 1, and slide 1 css

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //Add JS Here
+let i = 0;
+const txt = document.getElementById("introText").innerHTML;
+const speed = 35;
+
+//Adds Typewriter Effect
+document.getElementById("introText").innerHTML = "";
+
+function typeWriter() {
+
+  if (i < txt.length) {
+
+    document.getElementById("introText").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
